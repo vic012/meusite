3ef1f4f95160cdabcd51a266403cb2b79bb87c0c
Pedro Henrique <henriquevic012@gmail.com>
2023-07-17 19:45:13 +0000

    Update

--- config/static/js/blog_code.js
+++ config/static/js/blog_code.js
@@ -1,2 +1,24 @@
 //Javascript para o blog
-$('pre').attr('class', 'prettyprint');
+$(document).ready(function() {
+
+    $('.language-python').parent().attr('class', 'prettyprint');
+    $('.language-bash').parent().attr('class', 'prettybash');
+
+    // Cria duas novas <div>
+    var novaDivPython = $('<div class="python-command"></div>');
+    var novaDivBash = $('<div class="bash-command"></div>');
+
+    // Seleciona o trecho existente que deseja mover
+    var trechoExistentePython = $('.texto_postagem pre .language-python').parent();
+    var trechoExistenteBash = $('.texto_postagem pre .language-bash').parent();
+
+    // Adiciona a nova div como um elemento pai do trecho existente
+    trechoExistentePython.wrap(novaDivPython);
+    trechoExistenteBash.wrap(novaDivBash);
+
+    // Insere a nova div após o trecho existente
+    var novaDivTypeCodePython = $('<div class="type-code">Python >>></div>');
+    var novaDivTypeCodeBash = $('<div class="type-code">Bash >>></div>');
+    trechoExistentePython.prepend(novaDivTypeCodePython);
+    trechoExistenteBash.prepend(novaDivTypeCodeBash);
+});
