16a307cd5b376db5143b561f538b38b090e9c2eb
Pedro Henrique <pedro@pubdesign.com.br>
2023-07-18 08:34:35 -0300

    Fix js

--- static/js/blog_code.js
+++ static/js/blog_code.js
@@ -1,21 +1,17 @@
 //Javascript para o blog
+//$('pre').attr('class', 'prettyprint');
+$('.language-python').parent().attr('class', 'prettyprint');
+$('.language-bash').parent().attr('class', 'prettybash');
 $(document).ready(function() {
-
-    $('.language-python').parent().attr('class', 'prettyprint');
-    $('.language-bash').parent().attr('class', 'prettybash');
-
     // Cria duas novas <div>
     var novaDivPython = $('<div class="python-command"></div>');
     var novaDivBash = $('<div class="bash-command"></div>');
-
     // Seleciona o trecho existente que deseja mover
-    var trechoExistentePython = $('.texto_postagem pre .language-python').parent();
-    var trechoExistenteBash = $('.texto_postagem pre .language-bash').parent();
-
+    var trechoExistentePython = $('.texto_postagem pre .language-python');
+    var trechoExistenteBash = $('.texto_postagem pre .language-bash');
     // Adiciona a nova div como um elemento pai do trecho existente
     trechoExistentePython.wrap(novaDivPython);
     trechoExistenteBash.wrap(novaDivBash);
-
     // Insere a nova div após o trecho existente
     var novaDivTypeCodePython = $('<div class="type-code">Python >>></div>');
     var novaDivTypeCodeBash = $('<div class="type-code">Bash >>></div>');
